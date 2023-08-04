03b4cd9c8b506d5d45110015090a5ba2c30296b8
Rebecca Cottrell <r.e.cottrell@gmail.com>
2023-08-04 16:37:27 +0100

    Add JavaScript forms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,16 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+  const showHiddenContent = (event) => {
+    const contentToHide = event.target.parentElement;
+    contentToHide.style.display = 'none'
+
+    const switchContentBlock = contentToHide.parentElement;
+
+    const contentToShow = switchContentBlock.querySelector('.content-to-show');
+    contentToShow.style.display = 'block'
+  }
+
+  const contentToHide = document.querySelectorAll('.switch-content a');
+  contentToHide.forEach(content => content.addEventListener('click', showHiddenContent))
 })
